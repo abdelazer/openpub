639b9d125f4254bab6e4ea88ef87a8921f35aac9
abdelazer <abdelazer@cb300544-231c-11de-bf1e-7b5f7763e386>
2010-01-12 20:55:19 +0000

    Refine examples to actually pass with an embedded-Schematron-aware validator (now wired up in helper.py)

--- trunk/schemas/tests/test_opds_catalog_schema.py
+++ trunk/schemas/tests/test_opds_catalog_schema.py
@@ -4,15 +4,16 @@
 test_opds_catalog_schema.py
 
 Created by Keith Fahlgren on Tue Jan 12 06:48:18 PST 2010
-Copyright (c) 2010 Keith. All rights reserved.
+Copyright (c) 2010 Keith Fahlgren. All rights reserved.
 """
 
 import glob
 import logging
 import os.path
 
-from lxml import etree
 from nose.tools import *
+
+import helper
 
 log = logging.getLogger(__name__)
 
@@ -21,16 +22,15 @@ class TestOPDSCatalogSchema(object):
         self.testfiles_dir = os.path.join(os.path.dirname(__file__), 'files')
         self.schemas_dir = os.path.join(os.path.dirname(__file__), '..')
         self.schema_fn = os.path.join(self.schemas_dir, 'opds_catalog.rng')
-        self.validator = etree.RelaxNG(etree.parse(self.schema_fn))
+        self.validator = helper.RelamesValidator(self.schema_fn)
 
     def test_xml_files_pass_smoke(self):
         """All valid OPDS Catalog Documents collected for smoketesting should pass Relax NG validation."""
         for xml_fn in glob.glob(self.testfiles_dir + '/catalog*.pass.xml'):
             log.debug('Attempting validation of %s' % xml_fn)
-            xml = etree.parse(xml_fn)
             try:
-                self.validator.assertValid(xml)
-            except etree.DocumentInvalid:
+                self.validator.assertValid(xml_fn)
+            except helper.DocumentInvalid:
                 error_log = self.validator.error_log
                 log.warn('Validation errors:\n%s' % error_log)
                 raise AssertionError
